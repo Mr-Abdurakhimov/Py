768e9df29d24d01a702cb7e292b11a2a17f36b70
amir abdurakhimov <tiam11@bk.ru>
2023-10-13 12:56:53 +0300

    draw_field func was added

--- Practicum/nineteen.py
+++ Practicum/nineteen.py
@@ -1,2 +1,12 @@
 nums_str = (int(j) for i in range(1,20) for j in str(i) if i != 10)
 start_field = [[next(nums_str) for j in range(9)]for i in range(3)]
+
+def draw_field():
+    print("-" * 35)
+    for i in range(3):
+        for j in range(9):
+            print(f" {start_field[i][j]} ", end="|")
+        print()
+        print("-" * 35)
+
+draw_field()

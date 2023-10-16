9cd685d31d4147c169120ad2f28ebcc2a19ab3bb
amir abdurakhimov <tiam11@bk.ru>
2023-10-16 10:16:48 +0300

    draw_field func was updated

--- Practicum/bridges.py
+++ Practicum/bridges.py
@@ -8,6 +8,9 @@ field = [[" ", "O", " ", "O", " ", "O", " ", "O", " "],
          ["X", " ", "X", " ", "X", " ", "X", " ", "X"],
          [" ", "O", " ", "O", " ", "O", " ", "O", " "]]
 
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+my_dict = {j: i for i, j in enumerate(letters)}
+
 
 def draw_field():
     print("\t\t\t\t\t\t\t\tБерег Ноликов")
@@ -17,14 +20,17 @@ def draw_field():
             print("Берег крестиков", end=" |")
             for j in k:
                 print(f"\t{j}", end="")
-            print("\t|", end="")
-            print("  Берег крестиков")
+            print(f"\t| {i + 1}", end="")
+            print("   Берег крестиков")
         else:
             print("\t\t\t\t|", end="")
             for j in k:
                 print(f"\t{j}", end="")
-            print("\t|")
+            print("\t|", i + 1)
     print("\t\t\t\t" + "-" * 41)
+    print("\t\t\t\t\t", end="")
+    print(("\t").join(letters))
+    print()
     print("\t\t\t\t\t\t\t\tБерег ноликов")
 
 

016dc1876735f3145c3a33b140e25a3b672e4ac8
amir abdurakhimov <tiam11@bk.ru>
2023-10-16 11:26:51 +0300

    step functional was added

--- Practicum/bridges.py
+++ Practicum/bridges.py
@@ -1,3 +1,5 @@
+from colorama import Fore
+
 field = [[" ", "O", " ", "O", " ", "O", " ", "O", " "],
          ["X", " ", "X", " ", "X", " ", "X", " ", "X"],
          [" ", "O", " ", "O", " ", "O", " ", "O", " "],
@@ -10,6 +12,7 @@ field = [[" ", "O", " ", "O", " ", "O", " ", "O", " "],
 
 letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
 my_dict = {j: i for i, j in enumerate(letters)}
+players = {1: "X", 2: "O"}
 
 
 def draw_field():
@@ -34,4 +37,31 @@ def draw_field():
     print("\t\t\t\t\t\t\t\tБерег ноликов")
 
 
-draw_field()
+def check_step(line, row, sign):
+    if line - 1 >= 0 and line + 1 < 10:
+        if field[line - 1][row] == sign and field[line + 1][row] == sign:
+            field[line][row] = "|"
+            return
+    if row - 1 >= 0 and row + 1 < 10:
+        if field[line][row - 1] == sign and field[line][row + 1] == sign:
+            field[line][row] = "--"
+            return
+    print(f"Сюда нельзя ходить, потому что у {sign} нет пары, инициатива переходит сопернику")
+
+
+def make_step(coor, player):
+    sign = players[player]
+    row = my_dict[coor[0].lower()]
+    line = int(coor[1]) - 1
+    if field[line][row] == " ":
+        check_step(line, row, sign)
+    else:
+        print("К сожалению, вы сделали неправильный ход, инициатива переходит к сопернику")
+
+
+player = 1
+while True:
+    draw_field()
+    step = input("Сделайте ход: ")
+    make_step(step, player)
+    player = 2 if player == 1 else 1
